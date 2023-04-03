1d66a7ea5ebbe2ff4e5f10e26f4ae8d931f676d9
Vishwa <vishwdeep@outlook.com>
2023-04-03 19:00:09 +0530

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'root'
+)
+
+cursorObject = dataBase.cursor()
+
+
+cursorObject.execute("CREATE DATABASE djcrm")
+
+print("All Done")
